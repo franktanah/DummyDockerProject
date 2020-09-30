90dd235be447a7f3b0960ac0ac5104475519c1db
Frank Tan <franktanah@gmail.com>
2020-10-01 01:56:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const exp = require('express')
 const app = exp();
 
 app.get('/', (req,res) => {
-    res.send('dummy data')
+    res.send('Congratulations, project is successful')
 })
 
 app.listen(3000, () => {
